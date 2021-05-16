c9efdeff77d3795e9a5f97bb377aef3cddf345e0
Preston Jose <preston.daffodil@gmail.com>
2021-05-16 19:29:17 +0530

    Bot Config

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
+const discord = require('discord.js')
+const client = new discord.Client()
+require('dotenv').config()
+
+const port = 3000
 
 app.get('/', (req, res) => res.send('Welcome to SwiftJAB Server v0.1'))
 
-module.exports = app
+app.listen(port, () => console.log(`Server listening at port ${port}`))
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+	if(msg.content === 'hi') {
+		msg.reply('hello')
+	}
+});
+
+client.login(process.env.TOKEN)
